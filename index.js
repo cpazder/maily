88b691c1d2e49b1fa3d673f08759547488e5d7a4
cpazder <cpaz@chartermi.net>
2017-12-31 11:34:31 -0600

    refactor updates

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express');
+require('./services/passport');
+
+
 const app = express();
 
-app.get('/', (req, res) => {
-res.send({ bye: 'everyone' });
-});
+require("./routes/authRoutes")(app);
 
-// heroku target https://aqueous-temple-37398.herokuapp.com/
 
-// heroku git repo https://git.heroku.com/aqueous-temple-37398.git
+
 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+
+
+
+
+
+
+
+
+
+// heroku target https://aqueous-temple-37398.herokuapp.com/
+// heroku git repo https://git.heroku.com/aqueous-temple-37398.git
