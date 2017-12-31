7836dcc172827a9903a1a5215a714f71f68ce76c
cpazder <cpaz@chartermi.net>
2017-12-31 09:05:40 -0600

    small change for test

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-res.send({ hi: 'there' });
+res.send({ bye: 'everyone' });
 });
+
+// heroku target https://aqueous-temple-37398.herokuapp.com/
+
+// heroku git repo https://git.heroku.com/aqueous-temple-37398.git
 
 
 const PORT = process.env.PORT || 5000;
